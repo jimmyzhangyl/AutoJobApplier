f1948c309fad8c59c3b11b2e786bb487ab1fa2a4
Yuanlong Zhang <jimzhangyl@gmail.com>
2024-05-21 14:07:31 +1000

    improve UI

--- frontend/auto-job-applier/src/components/JobDetails.vue
+++ frontend/auto-job-applier/src/components/JobDetails.vue
@@ -4,19 +4,23 @@
     @click.self="close"
   >
     <div
-      class="bg-white p-6 rounded-lg shadow-lg max-w-2xl w-full max-h-[70%] overflow-y-auto"
+      class="bg-white p-6 rounded-lg shadow-lg max-w-2xl w-full max-h-[70%] flex flex-col"
     >
-      <h3 class="text-2xl font-bold mb-4">{{ job.title }}</h3>
-      <p class="text-lg mb-2"><strong>Location:</strong> {{ job.location }}</p>
-      <p class="text-lg mb-2">
-        <strong>Listing Date:</strong>
-        {{ new Date(job.listingDate).toLocaleDateString() }}
-      </p>
-      <div class="mb-4">
+      <div class="flex-grow-0">
+        <h3 class="text-2xl font-bold mb-4">{{ job.title }}</h3>
+        <p class="text-lg mb-2">
+          <strong>Location:</strong> {{ job.location }}
+        </p>
+        <p class="text-lg mb-2">
+          <strong>Listing Date:</strong>
+          {{ new Date(job.listingDate).toLocaleDateString() }}
+        </p>
+      </div>
+      <div class="flex-grow overflow-y-auto mb-4">
         <strong>Description:</strong>
         <pre class="whitespace-pre-wrap">{{ job.description }}</pre>
       </div>
-      <div class="flex justify-between items-center mt-4">
+      <div class="flex-grow-0 flex justify-between items-center">
         <a
           :href="job.applyLink"
           target="_blank"
